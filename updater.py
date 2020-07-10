0879314df24108e8eaacc54d3ec75e950381aa2d
Matteo <mateus.sakata@gmail.com>
2020-07-10 13:31:29 -0500

    inital commit

---
+++ updater.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+from git import Repo
+import os
+
+url = "https://api.globe.gov/search/v1/measurement/protocol/measureddate/?protocols=mosquito_habitat_mapper&startdate=2020-06-01&enddate=2020-07-04&geojson=FALSE&sample=TRUE"
+
+response = requests.get(url)
+globe_data = response.json()["results"]
+globe_data_df = pd.DataFrame(globe_data)
+data_df = pd.DataFrame(globe_data_df["data"].to_dict())
+data_df = data_df.transpose()
+mosquito_data_df = globe_data_df.join(data_df)
+mosquito_data_df.drop(["data"], axis=1)
+mosquito_data_df[["mosquitohabitatmapperLarvaeCount", "latitude", "longitude"]].fillna(0)
+
+
+mosquito_data_df.to_csv("mosquito_data.csv")

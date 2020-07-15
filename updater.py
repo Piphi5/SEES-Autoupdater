dea918679aa362cf34842a9ee2b6ed47d7cfdce5
Matteo <mateus.sakata@gmail.com>
2020-07-14 21:59:47 -0500

    cleaned code

--- updater.py
+++ updater.py
@@ -1,20 +1,10 @@
 import requests
 import pandas as pd
-from git import Repo
 import os
 from datetime import date
-from git import Actor
-
 
 today = date.today()
 url = f"https://api.globe.gov/search/v1/measurement/protocol/measureddate/?protocols=mosquito_habitat_mapper&startdate=2020-06-01&enddate={today}&geojson=FALSE&sample=FALSE"
-
-
-
-repo = Repo(os.getcwd())
-author = Actor("piphi5", "mateus.sakata@gmail.com")
-committer = Actor("A committer", "mateus.sakata@gmail.com")
-repo.heads.data.checkout()
 
 response = requests.get(url)
 globe_data = response.json()["results"]
@@ -25,15 +15,4 @@ mosquito_data_df = globe_data_df.join(data_df)
 mosquito_data_df.drop(["data"], axis=1)
 mosquito_data_df[["mosquitohabitatmapperLarvaeCount", "latitude", "longitude"]].fillna(0)
 
-
 mosquito_data_df.to_csv("mosquito_data.csv")
-
-file_list = [
-    "mosquito_data.csv"
-]
-commit_message = 'Automated data commit of GLOBE data'
-repo.index.add(file_list)
-repo.index.commit(commit_message)
-origin = repo.remote('origin')
-origin.push("data")
-repo.heads.master.checkout()

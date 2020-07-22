aa255ba44251ef0aedbd1431bab1efb10f2458ea
Matteo <mateus.sakata@gmail.com>
2020-07-22 15:01:50 -0500

    Grab Only SEES Intern Data
    The script iterates through a list of the GLOBE ID's of all NASA SEES Mosquito Mapper
    Interns and collects land cover and mosquito habitat mapper data from
    each of those users. It then combines this data and generates csv files
    containing the data.

--- updater.py
+++ updater.py
@@ -3,16 +3,25 @@ import pandas as pd
 import os
 from datetime import date
 
+ids = pd.read_csv('IDs.txt', sep=" ", header=None)
+id_list = (ids[0].tolist())
 today = date.today()
-url = f"https://api.globe.gov/search/v1/measurement/protocol/measureddate/?protocols=mosquito_habitat_mapper&startdate=2020-06-01&enddate={today}&geojson=FALSE&sample=FALSE"
+protocols = ["mosquito_habitat_mapper", "land_covers"]
 
-response = requests.get(url)
-globe_data = response.json()["results"]
-globe_data_df = pd.DataFrame(globe_data)
-data_df = pd.DataFrame(globe_data_df["data"].to_dict())
-data_df = data_df.transpose()
-mosquito_data_df = globe_data_df.join(data_df)
-mosquito_data_df.drop(["data"], axis=1)
-mosquito_data_df[["mosquitohabitatmapperLarvaeCount", "latitude", "longitude"]].fillna(0)
-
-mosquito_data_df.to_csv("mosquito_data.csv")
+for protocol in protocols:
+    main_df = pd.DataFrame()
+    for user_id in id_list:
+        url = f"https://api.globe.gov/search/v1/measurement/protocol/measureddate/userid/?protocols={protocol}&startdate=2020-06-01&enddate={today}&userid={user_id}&geojson=FALSE&sample=FALSE"
+        response = requests.get(url)
+        #print(response.content)
+        data = response.json()["results"]
+        
+        temp_data_df = pd.DataFrame(data)
+        if not temp_data_df.empty:
+            temp_data_df["data"]
+            data_df = pd.DataFrame(temp_data_df["data"].to_dict())
+            data_df = data_df.transpose()
+            temp_data_df = temp_data_df.join(data_df)
+            temp_data_df.drop(["data"], axis=1)
+            main_df = main_df.append(temp_data_df)
+    main_df.to_csv(f"results/{protocol}{today}.csv")
